b365e81de791129d0ecaf9794c4b657066f5d9c9
Norange01 <alrajabn@mcmaster.ca>
2025-12-28 20:35:31 -0500

    Remove registration and login links from bottom of form

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -193,12 +193,7 @@ export function SignUpForm({
                 {isLoading ? "Creating an account..." : "Sign up"}
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <Link href="/auth/login" className="underline underline-offset-4 text-sm">
-                Login
-              </Link>
-            </div>
+
           </form>
     </div>
   );
