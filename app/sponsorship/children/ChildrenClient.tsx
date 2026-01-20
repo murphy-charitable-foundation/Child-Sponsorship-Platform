f7c2eba4e27a99503aa3010752112a29a66cc7cb
Norange01 <alrajabn@mcmaster.ca>
2026-01-19 21:40:49 -0500

    Fix column name discrepancy

--- app/sponsorship/children/ChildrenClient.tsx
+++ app/sponsorship/children/ChildrenClient.tsx
@@ -2,23 +2,9 @@
 
 import React from "react";
 import { createClient } from "@/lib/supabase/server";
-//import {fetchChildren, getUniqueCountries} from "./fetch-children";
 import MeetTheChildrenUI from "@/components/meet-the-children-ui";
 import { Suspense } from "react";
 
-
-/*const children = [
-  { id: 1, name: "John Doe1", img: "/children/girl2.jpg", gender: "Male", age: 8, country: "Uganda1" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 2, name: "John Doe2", img: "/children/girl2.jpg", gender: "Male", age: 18, country: "Uganda" , grade: 4, bioText: "Short biography about the child goes here."},
-  { id: 3, name: "John Doe3", img: "/children/girl2.jpg", gender: "Female", age: 8, country: "Uganda1" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 4, name: "John Doe4", img: "/children/girl2.jpg", gender: "Male", age: 8, country: "Uganda2" , grade: 5, bioText: "Short biography about the child goes here."},
-  { id: 5, name: "John Doe5", img: "/children/girl2.jpg", gender: "Female", age: 20, country: "Uganda" , grade: 5, bioText: "Short biography about the child goes here."},
-  { id: 6, name: "John Doe6", img: "/children/girl2.jpg", gender: "Male", age: 21, country: "Uganda2" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 7, name: "John Doe7", img: "/children/girl2.jpg", gender: "Female", age: 8, country: "Uganda" , grade: 11, bioText: "Short biography about the child goes here."},
-  { id: 8, name: "John Doe8", img: "/children/girl2.jpg", gender: "Male", age: 11, country: "Uganda" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 9, name: "John Doe9", img: "/children/girl2.jpg", gender: "Male", age: 12, country: "Uganda2" , grade: 12, bioText: "Short biography about the child goes here."},
-
-];*/
 
 
 
@@ -30,13 +16,11 @@ const gradeLevels = ["Infant", "Pre-School", "1-3", "4-6", "7-9", "10-12", "Coll
     
 
 export default async function ChildrenClient() {
-  //const children = await fetchChildren(0, 4);
-  //const uniqueCountries = await getUniqueCountries();
 const supabase = await createClient();
 
     const { data: children, error: childrenError } = await supabase // TODO: pass and apply filters
         .from('children')
-        .select(`id, first_name, last_name, gender, date_of_birth, country, school_grade, photo_path, favorite_activity, dream_job`)
+        .select(`id, first_name, last_name, gender, date_of_birth, location, school_grade, photo_path, favorite_activity, dream_job`)
         .eq('active', true)
         .range(0, 5 - 1);
         //.order('id', { ascending: true });
@@ -49,8 +33,8 @@ const supabase = await createClient();
 
     const { data: countries, error: countriesError } = await supabase
         .from('children')
-        .select('country')
-        .neq('country', null)
+        .select('location')
+        .neq('location', null)
         .eq('active', true);
 
     if (countriesError) {
@@ -59,8 +43,11 @@ const supabase = await createClient();
     }
 
     const uniqueCountries = Array.from(
-        new Set(countries.map(row => row.country))
+        new Set(countries.map(row => row.location))
     ).sort();
+
+    console.log('Unique countries:', uniqueCountries);
+    console.log('Children:', children);
   
   return(
     <main>
--- components/meet-the-children-ui.tsx
+++ components/meet-the-children-ui.tsx
@@ -34,20 +34,6 @@ import { get } from "http";
 import { unique } from "next/dist/build/utils";
 
 
-/*const children = [
-  { id: 1, name: "John Doe1", img: "/children/girl2.jpg", gender: "Male", age: 8, country: "Uganda1" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 2, name: "John Doe2", img: "/children/girl2.jpg", gender: "Male", age: 18, country: "Uganda" , grade: 4, bioText: "Short biography about the child goes here."},
-  { id: 3, name: "John Doe3", img: "/children/girl2.jpg", gender: "Female", age: 8, country: "Uganda1" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 4, name: "John Doe4", img: "/children/girl2.jpg", gender: "Male", age: 8, country: "Uganda2" , grade: 5, bioText: "Short biography about the child goes here."},
-  { id: 5, name: "John Doe5", img: "/children/girl2.jpg", gender: "Female", age: 20, country: "Uganda" , grade: 5, bioText: "Short biography about the child goes here."},
-  { id: 6, name: "John Doe6", img: "/children/girl2.jpg", gender: "Male", age: 21, country: "Uganda2" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 7, name: "John Doe7", img: "/children/girl2.jpg", gender: "Female", age: 8, country: "Uganda" , grade: 11, bioText: "Short biography about the child goes here."},
-  { id: 8, name: "John Doe8", img: "/children/girl2.jpg", gender: "Male", age: 11, country: "Uganda" , grade: 3, bioText: "Short biography about the child goes here."},
-  { id: 9, name: "John Doe9", img: "/children/girl2.jpg", gender: "Male", age: 12, country: "Uganda2" , grade: 12, bioText: "Short biography about the child goes here."},
-
-];*/
-
-
 
 const genders = ["Male", "Female"];
 const gradeLevels = ["Infant", "Pre-School", "1-3", "4-6", "7-9", "10-12", "College"];
@@ -57,7 +43,6 @@ const gradeLevels = ["Infant", "Pre-School", "1-3", "4-6", "7-9", "10-12", "Coll
 
 export default function MeetTheChildrenUI({children, uniqueCountries}) {
   const router = useRouter(); // currently unused
-  //const [planType, setPlanType] = React.useState<PlanType>("monthly");
 
   const [selectedCountries, setCountries] = React.useState<Selection>(
     new Set(uniqueCountries)
@@ -68,10 +53,6 @@ export default function MeetTheChildrenUI({children, uniqueCountries}) {
 
   const [isLoaded, setIsLoaded] = React.useState(true);
 
-  /*const selectedValue = React.useMemo(
-    () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
-    [selectedKeys],
-  );*/
 
   return (
     <main className="min-h-screen bg-background text-foreground">
@@ -209,9 +190,10 @@ export default function MeetTheChildrenUI({children, uniqueCountries}) {
                 <CardFooter className="text-small">
                   <div className="text-left">
                   <b>{item.first_name+" "+item.last_name}</b>
-                  <p className="text-default-500">{"Age: "+("X").toString()+", "+item.country/*TODO: calculate age */}</p>
-                  <p className="text-default-500">{"Grade: "+(item.school_grade).toString()}</p>
-                  <p className="text-default-500">{"Dream job: "+item.dream_job+". "+"Favorite activity: "+item.favorite_activity+"."/* TODO: check for null*/}</p> 
+                  <p className="text-default-500"> Age: X{item.location ? `, ${item.location}` : ""/*TODO: calculate age */}</p>
+                  <p className="text-default-500">{"Grade: "+(item.school_grade ?? "N/A")}</p>
+                  <p className="text-default-500">{"Dream job: "+item.dream_job/* TODO: check for null*/}</p> 
+                  <p className="text-default-500">{"Favorite activity: "+item.favorite_activity/* TODO: check for null*/}</p> 
                   </div>
                 </CardFooter>
               </Skeleton>
