ed1cc3fbf9eacfc76d7369e628948f18c141156d
ZahraJafarifard <jafarifardz@yahoo.com>
2026-06-05 20:06:48 -0700

    Add dashboard UI

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardComp from "@/components/dashboard/page";
+
+const Dashboard = () => {
+  return (
+    <div className="">
+      <DashboardComp />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ components/dashboard/third-child.tsx
@@ -0,0 +1,45 @@
+const Card = ({
+  type,
+  text,
+  date,
+}: {
+  type: string;
+  text: string;
+  date: string;
+}) => {
+  return (
+    <article className="p-6 flex justify-between border-b border-[#111]/15">
+      <div>
+        <span
+          className={`rounded-sm py-2 px-3 me-[10px] ${type === "Payment" ? "text-[#900b09] bg-red-200" : "text-primary-500 bg-primary-200"}`}
+        >
+          {type}
+        </span>
+        <span className="text-lg font-medium">{text}</span>
+      </div>
+      <p className="text-default-700 text-lg">{date}</p>
+    </article>
+  );
+};
+
+const ThirdChild = () => {
+  return (
+    <section className="w-full mx-auto">
+      <h2 className="text-2xl font-semibold mb-6 text-primary-500">
+        Recent Activity
+      </h2>
+      <div className="p-6 rounded-3xl bg-white text-blue-900">
+        <Card type="Report" text="Monthly Report - David" date="Mar 15, 2026" />
+        <Card
+          type="Payment"
+          text="Payment Invoice - March 2016"
+          date="Apr 20, 2026"
+        />
+        <Card type="Report" text="Monthly Report - John" date="May 10, 2026" />
+        <Card type="Report" text="Monthly Report - Alex" date="Jun 5, 2026" />
+      </div>
+    </section>
+  );
+};
+
+export default ThirdChild;
