c2e1fdab386501aedeacbbd2822fc33def47a4b5
Aya <ayaaa.okzk@gmail.com>
2026-06-16 15:02:13 -0700

    Remove x-error

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,5 @@
 // app/admin/layout.tsx
 import React from "react";
-import { headers } from "next/headers";
 import { AdminSidebar } from "@/components/admin/layout/AdminSidebar";
 import AdminTopActions from "@/components/admin/layout/AdminTopActions";
 
@@ -9,12 +8,6 @@ export default async function AdminLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const headersList = await headers();
-	const error = headersList.get("x-error");
-	if (error) {
-		return <p className="text-red-500 mb-4">{error}</p>;
-	}
-
 	return (
 		<div className="h-screen bg-default-50 overflow-hidden">
 			<div className="flex h-full">
